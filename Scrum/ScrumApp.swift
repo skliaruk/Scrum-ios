9a847240b6f9aa559cacbf2073c67e48e7262ddc
Ilia Skliaruk <ilia.skliaruk@gmail.com>
2022-10-18 19:03:26 +0300

    Initial Commit

---
+++ Scrum/ScrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumApp.swift
+//  Scrum
+//
+//  Created by Ilia Skliaruk on 18.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
